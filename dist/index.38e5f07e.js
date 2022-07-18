6d50d1832781ed2857c651f21dd49eff10f4f13c
JunoField <junofield123@gmail.com>
2022-07-18 22:05:05 +0100

    done js for email validation, using regex

--- dist/index.38e5f07e.js
+++ dist/index.38e5f07e.js
@@ -1,6 +1,7 @@
 function validateEmail() {
     let email = document.getElementById("email-input").value;
-    email.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+    if (email.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) document.getElementById("email-input-section").classList.remove("email-section-error");
+    else document.getElementById("email-input-section").classList.add("email-section-error");
 }
 
 //# sourceMappingURL=index.38e5f07e.js.map
--- validateemail.js
+++ validateemail.js
@@ -2,9 +2,9 @@ function validateEmail(){
     let email = document.getElementById("email-input").value;
 
     if (email.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)){
-        //submission code here
+        document.getElementById("email-input-section").classList.remove("email-section-error")
     } else{
-        
+        document.getElementById("email-input-section").classList.add("email-section-error")
     }
 
 }
