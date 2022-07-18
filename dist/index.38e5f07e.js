07aeb0a5efe9e21e61c8378da51fed252f0d4b80
JunoField <junofield123@gmail.com>
2022-07-18 21:01:12 +0100

    started on js, having issues with the page not updatng

---
+++ dist/index.38e5f07e.js
@@ -0,0 +1,6 @@
+function validateEmail() {
+    let email = document.getElementById("email-input").value;
+    email.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+}
+
+//# sourceMappingURL=index.38e5f07e.js.map
