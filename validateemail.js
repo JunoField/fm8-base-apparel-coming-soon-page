d8fcbb325237a478db50b8f165737a02628ca84c
JunoField <junofield123@gmail.com>
2022-07-19 12:40:06 +0100

    got enter key working in js, added defer

--- validateemail.js
+++ validateemail.js
@@ -1,3 +1,4 @@
+
 function validateEmail(){
     let email = document.getElementById("email-input").value;
 
@@ -8,3 +9,18 @@ function validateEmail(){
     }
 
 }
+
+var input = document.getElementById("email-input");
+
+
+// Execute a function when the user presses a key on the keyboard
+input.addEventListener("keypress", function(event) {
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    validateEmail();
+  }
+}); 
+
